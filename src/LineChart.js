fcd1a72afe408fa319aec84504f7fdaa813eef8a
Gabriel Gibler <gabriel.gibler@pnnl.gov>
2022-12-21 19:11:57 -0800

    Update LineChart to current ncreview implementation. Switch App to LineChart.

--- src/LineChart.js
+++ src/LineChart.js
@@ -1,5 +1,5 @@
 // NPM packages
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef,  useEffect } from 'react';
 import * as d3 from 'd3';
 
 import {
@@ -7,6 +7,11 @@ import {
     diffColors,
     chartHeight, sharedLeftMargin
 } from './resources/constants';
+import {
+    secondsToReadableDate,
+} from './utils';
+
+diffColors['A'] = diffColors[nonDiff];
 
 
 // Source for using D3 in React:
@@ -25,76 +30,98 @@ const useD3Plot = (renderD3Plot) => {
 // https://observablehq.com/@d3/line-chart
 const LineChart = ({ plotData }) => {
 
-    let x = d => d[0]; // given d in data, returns the (temporal) x-value
-    let y = d => d[1]; // given d in data, returns the (quantitative) y-value
-    let defined = undefined; // for gaps in data
-    let curve = d3.curveLinear; // method of interpolation between points
-    let marginTop = 36; // top margin, in pixels
-    let marginRight = 18; // right margin, in pixels
-    let marginBottom = 36; // bottom margin, in pixels
-    let marginLeft = sharedLeftMargin; // left margin, in pixels
-    let width = 1400; // outer width, in pixels
-    let height = chartHeight; // outer height, in pixels
-    let xType = d3.scaleUtc; // the x-scale type
-    let xDomain = undefined; // [xmin, xmax]
-    let xRange = [marginLeft, width - marginRight]; // [left, right]
-    let yType = d3.scaleLinear; // the y-scale type
-    let dynamicY = false; // y-axis follows data? or always based at 0?
-    let yDomain = undefined; // [ymin, ymax]
-    let yRange = [height - marginBottom, marginTop]; // [bottom, top]
-    let yFormat = (d) => { // a format specifier string for the y-axis
-        if (d >= Math.pow(10, 6) || d <= -1 * Math.pow(10, 6)) {
-            return new Intl.NumberFormat('en-US', {style: 'decimal', notation: "compact", compactDisplay: "short"}).format(d);
-        }
-        else if (d <= Math.pow(10, -6) && d >= -1 * Math.pow(10, -6) && d !== 0) {
+    let diff = nonDiff;
+    let chartWidth = 1400; // outer width, in pixels
+    const margins = {
+        top: 30,
+        right: 30,
+        bottom: 30,
+        left: sharedLeftMargin,
+    };
+    const plotWidth = chartWidth - margins.left - margins.right;
+    const plotHeight = chartHeight - margins.top - margins.bottom;
+    const [ d3PlotConfig, ] = useState({
+        curve: d3.curveLinear, // method of interpolation between points
+        margins: margins,
+        plotWidth: plotWidth,
+        plotHeight: plotHeight,
+        xType: d3.scaleUtc, // the x-scale type
+        xRange: [margins.left, plotWidth + margins.left], // [left, right]
+        yType: d3.scaleLinear, // the y-scale type
+        yRange: [chartHeight - margins.bottom, margins.top], // [bottom, top]
+        dynamicY: false, // y-axis follows data? or always based at 0?
+        textColor: diffColors[same].dark, // color of axes labels and other text
+        axisFontSize: "1.222222em", // font-size of axes 
+        tooltipFontSize: "1.555555em", // font-size of tooltip text
+        strokeLinecap: "round", // stroke line cap of the line
+        strokeLinejoin: "round", // stroke line join of the line
+        strokeWidth: 2.0, // stroke width of line, in pixels
+        strokeOpacity: 1, // stroke opacity of line
+    });
+    const [ activeIndex, setActiveIndex ] = useState(-1);
+    const chartElemRefs = useRef({});
+
+    function getPlotColor(z) { // z == d.src
+        const _z = ( !z ? nonDiff : ( diff === added ? added : z ));
+        return diffColors[_z].line;
+    }
+
+    function yFormat(d) { // formats y-axis tick labels
+        if (d >= Math.pow(10, 12) || d <= -1 * Math.pow(10, 12) ||
+           (d <= Math.pow(10, -6) && d >= -1 * Math.pow(10, -6) && d !== 0))
+        {
             return d.toExponential(2);
+        }
+        else if (d >= Math.pow(10, 6) || d <= -1 * Math.pow(10, 6)) {
+            return new Intl.NumberFormat('en-US', {style: 'decimal', notation: "compact", compactDisplay: "short"}).format(d);
         }
         else {
             return d.toPrecision(7).replace(/0+$/,"");  // remove trailing zeros
         }
-    };
-    let yLabel = "Random Value ($)"; // a label for the y-axis
-    let textColor = "#babad2"; // color of axes labels and other text
-    let axisFontSize = "1.25em"; // font-size of axes labels
-    let strokeLinecap = "round"; // stroke line cap of the line
-    let strokeLinejoin = "round"; // stroke line join of the line
-    let strokeWidth = 2.0; // stroke width of line, in pixels
-    let strokeOpacity = 1; // stroke opacity of line
-    let mixBlendMode = "multiply"; // blend mode of lines
+    }
+
+    // Compute values.
+    const X = d3.map(plotData, d => d.x); // Array of times
+    const Y = d3.map(plotData, d => d.y); // Array of data values
+    const Z = d3.map(plotData, d => d.src); // Array of the "category" -- d.src (A|B), in this case.
+    let defined = d => !( // identifies gaps in data
+        [null, undefined].includes(d.x) ||
+        isNaN(d.x) ||
+        [null, undefined].includes(d.y) ||
+        isNaN(d.y)
+    );
+    const D = d3.map(plotData, defined); // Array of true|false
+
+    // Compute default domains, and unique the z-domain.
+    let xDomain = d3.extent(X); // [xmin, xmax]
+    let yDomain = (() => { // [ymin, ymax]
+        let yMin = ( d3PlotConfig.dynamicY ? d3.min(Y) : d3.min([0, d3.min(Y)]) );
+        let yMax = d3.max(Y, d => typeof d === "string" ? +d : d);
+        let yPadding = (yMax - yMin) * .02;
+        yMin = yMin - yPadding;
+        yMax = yMax + yPadding;
+        return [yMin, yMax];
+    })();
+    let zDomain = new d3.InternSet(Z); // Exactly as expected: Z reduced to a set of 2
+
+    // Omit any data not present in the z-domain.
+    const I = d3.range(X.length).filter(i => zDomain.has(Z[i])); // Array of values 0 -> X.length - 1, continuous ?unless Z has bad values?
+
+    // Construct scales for axes.
+    const xScale = d3PlotConfig.xType(xDomain, d3PlotConfig.xRange);
+    const xScaleMax = xScale(X[X.length-1]);
+    const yScale = d3PlotConfig.yType(yDomain, d3PlotConfig.yRange);
+
 
     const chartRef = useD3Plot((svg) => {
+        const cfg = d3PlotConfig;
 
-        // Compute values.
-        const X = d3.map(plotData.A, x);
-        const Y = d3.map(plotData.A, y);
-        const I = d3.range(X.length);
-        if (defined === undefined) defined = (d) => !(
-            [null, undefined].includes(d[0]) ||
-            isNaN(d[0]) ||
-            [null, undefined].includes(d[1]) ||
-            isNaN(d[1])
-        );
-        const D = d3.map(plotData.A, defined);
-
-        // Compute default domains.
-        if (xDomain === undefined) xDomain = d3.extent(X);
-        if (yDomain === undefined) {
-            let yMin = ( dynamicY ? d3.min(Y) : d3.min([0, d3.min(Y)]) );
-            let yMax = d3.max(Y);
-            let yPadding = (yMax - yMin) * .02;
-            yMin = yMin - yPadding;
-            yMax = yMax + yPadding;
-            yDomain = [yMin, yMax];
-        }
-
-        // Construct scales and axes.
-        const xScale = xType(xDomain, xRange);
-        const yScale = yType(yDomain, yRange);
+        // Construct axes.
         const xAxis = d3.axisBottom(xScale)
-            .ticks(width / (width/12))
+            .ticks(chartWidth / (chartWidth/12))
             .tickSizeOuter(0);
         const yAxis = d3.axisLeft(yScale)
-            .ticks(height / (height/10))
+            .ticks(chartHeight / (chartHeight/10))
             .tickSizeInner(4)
             .tickFormat(yFormat)
             .tickPadding(9);
@@ -102,65 +129,107 @@ const LineChart = ({ plotData }) => {
         // Construct a line generator.
         const line = d3.line()
             .defined(i => D[i])
-            .curve(curve)
+            .curve(cfg.curve)
             .x(i => xScale(X[i]))
             .y(i => yScale(Y[i]));
 
         svg
-            .attr("width", width)
-            .attr("height", height)
-            .attr("viewBox", [0, 0, width, height])
-            .attr("style", "max-width: 100%; height: auto; height: intrinsic; background-color: #282c34;")
+            .attr("viewBox", [0, 0, chartWidth, chartHeight])
+            .attr("style", "max-width: 100%; height: auto; height: intrinsic; background-color: #282c34; -webkit-tap-highlight-color: transparent;")
             .selectAll('*').remove();
 
         svg.append("g")
-            .attr("transform", `translate(0,${height - marginBottom})`)
+            .attr("class", "axis x-axis")
+            .attr("transform", `translate(0,${chartHeight - margins.bottom})`)
             .call(xAxis)
-            .style("color", textColor)
+            .style("color", cfg.textColor)
             .selectAll("text")
-                .style("font-size", axisFontSize);
+                .style("font-size", cfg.axisFontSize);
 
         svg.append("g")
-            .attr("transform", `translate(${marginLeft * .926},0)`)
+            .attr("class", "axis y-axis")
+            .attr("transform", `translate(${margins.left * .926},0)`)
             .call(yAxis)
-            .style("color", textColor)
+            .style("color", cfg.textColor)
             .call(g => g.select(".domain").remove())
             .call(g => g.selectAll(".tick line").clone()
-                .attr("x2", width - marginLeft - marginRight)
+                .attr("x2", plotWidth)
                 .attr("stroke-opacity", 0.1))
-            .call(g => g.append("text")
-                .attr("x", 0)
-                .attr("y", axisFontSize)
-                .attr("fill", "green")
-                .attr("text-anchor", "end")
-                .text(yLabel))
             .selectAll("text")
-                .style("font-size", axisFontSize);
+                .style("font-size", cfg.axisFontSize);
 
-        svg.append("path")
+        const paths = svg.append("g")
+            .attr("class", "plotPaths")
             .attr("fill", "none")
-            .attr("stroke", diffColors.A.line)
-            .attr("stroke-width", strokeWidth)
-            .attr("stroke-linecap", strokeLinecap)
-            .attr("stroke-linejoin", strokeLinejoin)
-            .attr("stroke-opacity", strokeOpacity)
-            .attr("d", line(I));
+            .attr("stroke-width", cfg.strokeWidth)
+            .attr("stroke-linecap", cfg.strokeLinecap)
+            .attr("stroke-linejoin", cfg.strokeLinejoin)
+            .attr("stroke-opacity", cfg.strokeOpacity)
+            .selectAll("path")
+            .data(d3.group(I, i => Z[i]))
+            .join("path")
+                .attr("class", ([z]) => `plotLine${z} plotPath${z}`)
+                .attr("stroke", ([z]) => getPlotColor(z))
+                .attr("d", ([, I]) => line(I));
+        chartElemRefs.current['paths'] = paths;
 
-        const plotPtsA = d3.filter(plotData.A, d => defined(d));
-        svg.selectAll(".plotPtsA")
-            .data(plotPtsA)
-            .join("circle")
-              .attr("class", "plotPtsA")
-              .attr("fill", diffColors.A.line)
-              .attr("stroke", "none")
-              .attr("r", strokeWidth)
-              .attr("cx", d => xScale(d[0]))
-              .attr("cy", d => yScale(d[1]));
+        const tooltip = svg.append("g")
+            .attr("class", "plotTooltip")
+            .attr("display", "none");
+        tooltip.append("circle")
+                .attr("fill", "none")
+                .attr("stroke", cfg.textColor)
+                .attr("r", cfg.strokeWidth * 1.666667);
+        tooltip.append("text")
+                .attr("fill", cfg.textColor);
+        if (activeIndex > -1) {
+            tooltip.attr("display", null);
+            updateTooltip(activeIndex, tooltip);
+        }
+        chartElemRefs.current['tooltip'] = tooltip;
+
+        return svg;
     });
+
+    function updateTooltip(i, tooltip) {
+        const x = xScale(X[i]);
+        const y = yScale(Y[i]);
+        if ((x === 0 || x) && (y === 0 || y)) {
+            tooltip.attr("transform", `translate(${x},${y})`);
+            const tooltipText = tooltip.select("text");
+            const tooltipTextText = secondsToReadableDate(X[i]/1000);
+            tooltipText.text(tooltipTextText)
+                .attr("transform", `translate(0,-${Math.floor(y - 21)})`) // Arbitrary portion of margins.top //${plotHeight - yScale(Y[i])})`) // Overlays it on the x-axis
+                .attr("text-anchor", ( x < xScaleMax*0.125 ? "start" : ( x > xScaleMax*0.875 ? "end" : "middle" )));
+        }
+    }
+
+    // Events may fire before render
+    function pointermoved(event) {
+        const tooltip = chartElemRefs.current['tooltip'];
+        if (tooltip) {
+            const [xm, ym] = d3.pointer(event);
+            const i = d3.least(I, i => Math.hypot(xScale(X[i]) - xm, yScale(Y[i]) - ym)); // closest point
+            updateTooltip(i, tooltip);
+        }
+    }
+
+    function pointerentered() {
+        const tooltip = chartElemRefs.current['tooltip'];
+        if (tooltip) tooltip.attr("display", null);
+    }
 
 
     return (
-        <svg ref={ chartRef }></svg>
+        <svg
+            ref={ chartRef }
+            style={{ width: '100%', height: `${chartHeight}px`, margin: 0, backgroundColor: '#1a1a1a' }}
+            width={ chartWidth }
+            height={ chartHeight }
+            onPointerEnter={ pointerentered }
+            onPointerMove={ pointermoved }
+            onTouchStart={ event => event.preventDefault() }
+        ></svg>
     );
 };
 
